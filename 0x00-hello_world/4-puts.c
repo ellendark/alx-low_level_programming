35d78c3ea7efa26c538a282a41c302f326dd081b
ellendark <epokua62@gmail.com>
2023-03-16 22:48:23 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/*
+ * main - print a line of code using puts
+ *
+ * Return:0
+*/
+print main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle"):
+	return(o):
+}
