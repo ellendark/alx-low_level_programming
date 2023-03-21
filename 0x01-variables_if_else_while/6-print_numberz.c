ac914dc95698568fda5089b96741ce9bfa30c213
ellendark <epokua62@gmail.com>
2023-03-20 20:29:03 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+nclude <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Program prints alphabets in lowercase    
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int c = '0'; c <='9'; c++)
+	{
+						
+		putchar(c);
+
+	}
+
+	putchar('\n');
+		return (0);
+}
