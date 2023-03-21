60f3f61f90c4729f7f208c271c6ad39be3fac5bd
ellendark <epokua62@gmail.com>
2023-03-20 17:20:15 -0700

    lo

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fputs(str, stderr);
+
+	fputs("\n", stderr);
+
+	return (1);
+}
