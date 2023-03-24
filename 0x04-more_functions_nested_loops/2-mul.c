8d71e7cc2c2a452e90b664176c41216116645e9f
ellendark <epokua62@gmail.com>
2023-03-24 10:17:58 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiples two numbers
+ * @a:firt integer value
+ * @b: second integer value
+ * Return: mult (product of two number)
+ * 
+ */
+
+int mul(int a, int b)
+{
+	/** assign arguments to formal parameter*/
+	return (a * b);
+}
