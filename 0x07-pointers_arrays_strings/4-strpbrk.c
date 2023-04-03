55bb070cb362f631e09796854f348ec166261124
ellendark <epokua62@gmail.com>
2023-04-03 14:07:06 -0700

    4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *@s: input
+ *@accept: input
+ *Return: Always 0 (succes)
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+		if (*s == accept[k])
+		return (s);
+		}
+	s++;
+	}
+	return ('\0');
+}
